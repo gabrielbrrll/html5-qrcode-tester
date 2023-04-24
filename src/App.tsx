8439711cd456c0a746176997dfd7f1e9a820cf00
Gab Ocampo <gabrielb.ocampo@gmail.com>
2023-04-24 13:49:42 +0800

    app tester implementation html5-qrcode

---
+++ src/App.tsx
@@ -0,0 +1,152 @@
+import { CameraDevice, Html5Qrcode, Html5QrcodeCameraScanConfig, Html5QrcodeScannerState } from 'html5-qrcode';
+import { Html5QrcodeConfigs } from 'html5-qrcode/esm/html5-qrcode';
+import { useCallback, useEffect, useState } from 'react'
+
+import './App.css'
+
+interface ScannerConfigs extends Html5QrcodeCameraScanConfig, Html5QrcodeConfigs {
+  rememberLastUsedCamera: boolean;
+  focusMode: string;
+  defaultZoomValueIfSupported: number;
+}
+
+export const defaultHtml5QrCodeConfigs: ScannerConfigs = {
+  fps: 200,
+  rememberLastUsedCamera: true,
+  disableFlip: false,
+  focusMode: "continuous",
+  defaultZoomValueIfSupported: 1,
+  aspectRatio: 1,
+  qrbox: {
+      height: 350,
+      width: 350,
+  },
+  useBarCodeDetectorIfSupported: true,
+};
+
+type CameraId = CameraDevice["id"];
+
+const App = () => {
+  const [scannerState, setScannerState] = useState(Html5QrcodeScannerState.NOT_STARTED);
+  const [scanner, setScanner] = useState<Html5Qrcode | null>(null);
+  const [cameras, setCameras] = useState<CameraDevice[]>([])
+  const [activeCameraId, setActiveCameraId] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onHandleScanSuccess = (decodedText: string) => {
+    alert(decodedText)
+  }
+
+  useEffect(() => {
+    const fetchCameraDevices = async () => {
+        try {
+            const devices = await Html5Qrcode.getCameras();
+            console.log(devices, '++DEVICES')
+            setCameras(devices);
+        } catch (err) {
+            console.warn("camera devices fetch failed", err);
+        }
+    };
+
+    void fetchCameraDevices();
+}, []);
+
+
+  const enableScanner = useCallback(
+    async (deviceId?: CameraId) => {
+        let cameraId: CameraId;
+
+        try {
+            /* istanbul ignore else */
+            if (cameras && cameras.length > 0) {
+               if (deviceId && cameras.some((camera) => camera.id === deviceId)) {
+                    cameraId = deviceId;
+                } else {
+                    cameraId = cameras[0].id;
+                }
+                console.log("PASOk", cameras)
+                console.log("deviceId", deviceId)
+                console.log("cameraId", cameraId)
+
+                const html5QrCode = new Html5Qrcode("qr-scanner");
+                const configs = defaultHtml5QrCodeConfigs;
+
+                const startScanner = async () => {
+                    await html5QrCode.start(
+                        cameraId,
+                        {
+                            ...configs,
+                        },
+                        (decodedText) => {
+                            onHandleScanSuccess(decodedText);
+                        },
+                        (err) => console.warn("no qr code parsed", err)
+                    );
+
+                    console.log(cameraId, '+ITO UNG CAMERA ID SA START SCANNER');
+                };
+
+                console.log(scannerState, '++SCANNER STATE')
+
+                /* istanbul ignore else */
+                if (scannerState === Html5QrcodeScannerState.NOT_STARTED || scannerState === Html5QrcodeScannerState.SCANNING) {
+                    await startScanner();
+                    setIsLoading(false);
+                    setScanner(html5QrCode);
+                    setScannerState(Html5QrcodeScannerState.SCANNING);
+                } 
+            }
+        } catch (err) {
+            console.warn("scanner init failed", err);
+        }
+      },
+      [
+          onHandleScanSuccess,
+          scannerState,
+      ]
+  );
+
+  useEffect(() => {
+    if (scannerState === Html5QrcodeScannerState.NOT_STARTED && !activeCameraId) {
+        void enableScanner();
+    }
+  }, [enableScanner, scannerState]);
+
+  const stopCamera = async () => {
+    if (scanner && scannerState === Html5QrcodeScannerState.SCANNING) {
+      await scanner?.stop();
+      scanner?.clear();
+      setScanner(null)
+      setScannerState(Html5QrcodeScannerState.NOT_STARTED);
+    }
+  }
+
+  const handleCameraChange = async (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setIsLoading(true);
+    const cameraId = event.target.value;
+
+    setActiveCameraId(cameraId);
+
+    await stopCamera();
+
+    await enableScanner(cameraId)
+  };
+
+  return (
+    <div className="App">
+      <h1>Scanner app proto</h1>
+      <div id="qr-scanner"></div>
+      <select value={activeCameraId} onChange={handleCameraChange}>
+        {cameras.map((camera) => (
+          <option key={camera.id} value={camera.id}>
+            {camera.label}
+          </option>
+        ))}
+      </select>
+      {/* <button onClick={stopCamera}>Stop camera</button> */}
+      {isLoading && <div>Is loading</div>}
+    </div>
+  )
+} 
+
+export default App
